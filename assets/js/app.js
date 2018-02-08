e84c22d864b3be0259965783c33b81feef951799
vanemarnava <nechan10@gmail.com>
2018-02-08 09:57:45 -0300

    subiendo eslint

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,49 +1,39 @@
-$(document).ready(function(){
-
+$(document).ready(function() {
 // INICIAR SESION
-//validar no estar vacio
-//validar password no sea 123456
-//minimo permitido 6 caracteres
+  $('#btnSign').click(function() {
+    saveLocaStorage();
+    function saveLocaStorage() {
+      let user = $('#user').val();
+      var passVal = $('#passInput').val();
+      localStorage.setItem('user', user);
+      localStorage.setItem('passVal', passVal);
+    }
 
-	$('#btnSign').click(function(){
-		saveLocaStorage();
-		function saveLocaStorage() {
-			let user = $('#user').val();
-			var passVal = $('#passInput').val();
-			localStorage.setItem('user', user);
-			localStorage.setItem('passVal', passVal);
-		}
+    var passVal = $('#passInput').val();
 
-		var passVal = $('#passInput').val();
-
-			// la contrasena no debe estar vacia, debe tener mas de 6 caracteres, no debe ser 123456
-			if (passVal !== '' && passVal.length >= 6 && passVal !== '123456') {
-				window.location = 'gallery.html';
-			} else {
-				alert('Ingrese contrasena valida.');
-			}
-
-		});
-
+    // la contrasena no debe estar vacia, debe tener mas de 6 caracteres, no debe ser 123456
+    if (passVal !== '' && passVal.length >= 6 && passVal !== '123456') {
+      window.location = 'gallery.html';
+    } else {
+      alert('Ingrese contrasena valida.');
+    }
+  });
 });
 
 
 // MOVER LAS FOTOS DE UN CONTENEDOR A OTRO
-// 
-function drag(ev) {
-  console.log(ev.target.src);
-  ev.dataTransfer.setData('text', ev.target.id);
+function drag(evento) {
+  console.log(evento.target.src);
+  evento.dataTransfer.setData('text', evento.target.id);
 }
 
-// 
-function allowDrop(ev){
-  ev.preventDefault();
+function allowDrop(evento) {
+  evento.preventDefault();
 }
 
-// 
-function drop(ev) {
-  ev.preventDefault();
-  var id_foto = ev.dataTransfer.getData('text');
-  ev.target.appendChild(document.getElementById(id_foto));
+function drop(evento) {
+  evento.preventDefault();
+  var idFoto = evento.dataTransfer.getData('text');
+  evento.target.appendChild(document.getElementById(idFoto));
 }
 
--- assets/js/datas.js
+++ assets/js/datas.js
@@ -1,12 +1,12 @@
 getLocalStorage();
-		function getLocalStorage() {
-			if(localStorage.getItem('passVal')) {
-				let passVal = localStorage.getItem('passVal');
-				let user = localStorage.getItem('user');
-				$('#userName').append(user);
-			} else {
-				console.log('no hay info guardada');
-			}	
 
-		}
+function getLocalStorage() {
+  if (localStorage.getItem('passVal')) {
+    let passVal = localStorage.getItem('passVal');
+    let user = localStorage.getItem('user');
+    $('#userName').append(user);
+  } else {
+  	console.log('no hay info guardada');
+  }	
+}
 		
